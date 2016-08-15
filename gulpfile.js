4baf040c33c63c30bbf5a1da13688467c14d7b99
Artem_Koroteev <artem_koroteev@epam.com>
2016-08-15 17:13:18 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+﻿var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var server = require('gulp-express');
+
+gulp.task('ts', function () {
+    gulp.src('./client/ts/*.ts')
+    .pipe(ts({
+        noImplicitAny: true
+    }))
+    .pipe(gulp.dest('./client/js/'));
+});
+
+gulp.task('server', function () {
+    server.run(['server.js'], {}, false);
+    gulp.watch('./client/ts/*.ts', ['ts']);
+});
+
+gulp.task('default', ["server"]);
